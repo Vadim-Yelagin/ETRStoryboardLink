73257ba5fe0bd7fa4cb869bc0614c8f0cf93a510
Vadim Yelagin <v.yelagin@eastbanctech.ru>
2015-01-14 15:49:55 +0600

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,9 @@
+//
+//  ETRStoryboardLinkTests.m
+//  ETRStoryboardLinkTests
+//
+//  Created by Vadim Yelagin on 01/14/2015.
+//  Copyright (c) 2014 Vadim Yelagin. All rights reserved.
+//
+
+${TEST_EXAMPLE}
